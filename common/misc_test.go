c53a67e7dd86e5e7a253e1a5f688a1d4406fadf2
Bruce Wang <byyw13@gmail.com>
2017-10-18 13:09:30 -0400

    add some tests

--- common/misc_test.go
+++ common/misc_test.go
@@ -6,8 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestParseType(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal("errored", ParseType("Error"))
+	assert.Equal("count", ParseType("ct"))
+	assert.Equal("skipped", ParseType("SKiP"))
+	assert.Equal("already", ParseType("already"))
+}
+
 func TestRet(t *testing.T) {
 	assert := assert.New(t)
 	ret := NewRet("test")
 	assert.Equal(0, ret.Value("count"))
+	ret.Add("count")
+	assert.True(ret.Eq("count", 1))
+	assert.True(ret.Gte("count", 1))
+	assert.True(ret.Lte("count", 1))
+	assert.False(ret.Lt("count", 1))
+	assert.False(ret.Gt("count", 1))
 }
