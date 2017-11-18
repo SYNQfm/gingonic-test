0bbeb965e9e16d6d37bd0789bd81be878a86057f
Bruce Wang <byyw13@gmail.com>
2017-11-17 21:54:04 -0800

    call common

--- ext/setup.go
+++ ext/setup.go
@@ -5,11 +5,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-const (
-	DEFAULT_DB_URL = "postgres://circleci:circleci@localhost:5432/db_test?sslmode=disable"
-)
-
 func SetupDB(def_url ...string) *sqlx.DB {
-	dbaddr := common.GetDB()
+	dbaddr := common.GetDB(def_url...)
 	return sqlx.MustConnect("postgres", dbaddr)
 }
