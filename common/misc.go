08a7c3622513fc5ea88185afce84580883fc23b5
Bruce Wang <byyw13@gmail.com>
2017-12-11 17:26:45 -0800

    add tests

--- common/misc.go
+++ common/misc.go
@@ -198,7 +198,3 @@ func getMultipartSignature(headers, awsSecret string) []byte {
 func getFileExtension(filetype string) string {
 	return strings.Split(strings.Split(filetype, "/")[1], "+")[0]
 }
-
-func getExpiration() string {
-	return time.Now().Add(time.Hour * time.Duration(24)).Format(TimeFormat)
-}
