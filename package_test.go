463e819e16fe0b53a19082b2c28d900f8993f83c
Bruce Wang <byyw13@gmail.com>
2018-01-04 13:35:55 -0500

    remove setup tests

--- package_test.go
+++ package_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/SYNQfm/helpers/common"
-	"github.com/SYNQfm/helpers/ext"
 	"github.com/SYNQfm/helpers/test_helper"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,10 +11,6 @@ import (
 func TestImport(t *testing.T) {
 	resp := test_helper.Response{}
 	assert.NotNil(t, resp)
-	api := ext.SetupSynqApi()
-	assert.Equal(t, "v1", api.Version())
-	api2 := ext.SetupSynqV2()
-	assert.Equal(t, "v2", api2.Version())
 	err := common.NewError("test error %s", "val")
 	assert.Equal(t, "test error val", err.Error())
 }
